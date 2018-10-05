8757020facd91485c4dd1d9ffcf093e117b07276
Connor Peebles <connorpeebles93@gmail.com>
2018-10-05 22:54:49 +0000

    ships now properly appear one at a time, but can still overlap

--- battleship.js
+++ battleship.js
@@ -2,29 +2,36 @@ var fleet = [
   {
     name: "carrier",
     numCoords: 5,
-    coords: []
+    coords: [],
+    jq: $("<div>").attr("id", "carrier")
   },
   {
     name: "battleship",
     numCoords: 4,
-    coords: []
+    coords: [],
+    jq:$("<div>").attr("id", "battleship")
   },
   {
     name: "cruiser",
     numCoords: 3,
-    coords: []
+    coords: [],
+    jq: $("<div>").attr("id", "cruiser")
   },
   {
     name: "submarine",
     numCoords: 3,
-    coords: []
+    coords: [],
+    jq: $("<div>").attr("id", "submarine")
   },
   {
     name: "destroyer",
     numCoords: 2,
-    coords: []
+    coords: [],
+    jq: $("<div>").attr("id", "destroyer")
   }
 ];
+
+var lol = [];
 
 function fillGrid(grid) {
 
@@ -56,28 +63,29 @@ function fillGrid(grid) {
 
 }
 
-function placeShips(shipName, numTiles, index, grid) {
+function placeShips(fleet, grid) {
 
-  if (index === 4) {
-    $("#instructionBox").text("Done");
-    return null;
+  if (fleet === []) {
+    return;
   }
 
-  $("#instructionBox").text("Use your mouse to drag and place your \"" + shipName + "\" ship. Double-click the ship with your mouse to switch the ship's orientation. Press any key to confirm the ship's location.");
+  var ship = fleet[0].jq;
+  var shipName = fleet[0].name;
+  var numTiles = fleet[0].numCoords;
 
-  var ship = $("<div>").attr("id",shipName).addClass("ship");
-  $(ship).css({width: 60*numTiles-2});
+  $("#instructionBox").text("Use your mouse to drag and place your \"" + shipName + "\" ship. Double-click the ship with your mouse to switch the ship's orientation. Press Enter to confirm the ship's location.");
+
   $(grid).append(ship);
 
   var vertical = false;
 
-  $("#"+shipName).draggable({opacity: 0.6, grid: [60,60], containment: grid});
+  $(ship).draggable({opacity: 0.6, grid: [60,60], containment: grid});
 
-  $("#"+shipName).dblclick(function() {
-    var Top = parseInt($(this).css("top"));
-    var Left = parseInt($(this).css("left"));
-    var tempH = parseInt($(this).css("height"));
-    var tempW = parseInt($(this).css("width"));
+  $(ship).dblclick(function() {
+    var Top = parseInt($(ship).css("top"));
+    var Left = parseInt($(ship).css("left"));
+    var tempH = parseInt($(ship).css("height"));
+    var tempW = parseInt($(ship).css("width"));
 
     if (Top + tempW > 655) {
       Top = 660 - (60 * numTiles);
@@ -86,45 +94,66 @@ function placeShips(shipName, numTiles, index, grid) {
       Left = 660 - (60 * numTiles);
     }
     vertical = !vertical;
-    $(this).css({height: tempW, width: tempH, top: Top, left: Left});
+    $(ship).css({height: tempW, width: tempH, top: Top, left: Left});
   });
 
-  $(document).keypress(function() {
-    var Top = Math.round(parseInt($("#"+shipName).css("top")) / 60);
-    var Left = Math.round(parseInt($("#"+shipName).css("left")) / 60);
-
-    if (Top === 0 || Left === 0 || vertical && Top > (11 - numTiles) || !vertical && Left > (11 - numTiles)) {
-      $("#instructionBox").text("Please place your " + shipName + " within the confines of the grid (on the blue squares).");
-
-    }
-    else {
-      for (var i = 0; i < numTiles; i++) {
-        if (vertical) {
-          fleet[index].coords.push([Left, Top + i]);
-        } else {
-          fleet[index].coords.push([Left + i, Top]);
-        }
-      }
+  $(document).keypress(function(e) {
+    if(e.which === 13) {
       $("#"+shipName).off();
-      $(document).off();
-      return null;
-  }
+      // lol.push(index);
+      placeShips(fleet.slice(1), grid);
+    }
+    // var Top = Math.round(parseInt($("#"+shipName).css("top")) / 60);
+    // var Left = Math.round(parseInt($("#"+shipName).css("left")) / 60);
+
+    // if (Top === 0 || Left === 0 || vertical && Top > (11 - numTiles) || !vertical && Left > (11 - numTiles)) {
+    //   $("#instructionBox").text("Please place your " + shipName + " within the confines of the grid (on the blue squares).");
+
+    // }
+    // else {
+    //   for (var i = 0; i < numTiles; i++) {
+    //     if (vertical) {
+    //       fleet[index].coords.push([Left, Top + i]);
+    //     } else {
+    //       fleet[index].coords.push([Left + i, Top]);
+    //     }
+    //   }
+    //   return null;
+  // }
   });
+// }
 }
 
 function playBattleship() {
+
   var userGrid = $("#userGrid")[0];
 
   fillGrid(userGrid);
 
-  placeShips(fleet[0].name, fleet[0].numCoords, 0, userGrid);
+  // var carrier = $("<div>").attr("id", "carrier");
+  // var battleship = $("<div>").attr("id", "battleship");
+  // var cruiser = $("<div>").attr("id", "cruiser");
+  // var submarine = $("<div>").attr("id", "submarine");
+  // var destroyer = $("<div>").attr("id", "destroyer");
+
+  // fleet[0].jq = carrier;
+  // fleet[1].jq = battleship;
+  // fleet[2].jq = cruiser;
+  // fleet[3].jq = submarine;
+  // fleet[4].jq = destroyer;
+
+  placeShips(fleet, userGrid);
 
   // placeShips(fleet[1].name, fleet[1].numCoords, 1, userGrid);
 
+  // var timeoutID = setTimeout(function() {
+  //   placeShips(fleet[1].name, fleet[1].numCoords, 1, userGrid);
+  // }, 1000*10);
+
 }
 
-function click(grid) {
-  $(".coord").click(function(){
-    $(this).css({background: "#FF0000"});
-  });
-}
+// function click(grid) {
+//   $(".coord").click(function(){
+//     $(this).css({background: "#FF0000"});
+//   });
+// }
